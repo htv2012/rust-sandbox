75195ce88dfa549a6042e2402d6bfc858f942a5f
Hai Vu <haivu2004@gmail.com>
2024-10-17 14:44:46 -0700

    Add comments to  variables

--- topics/variables/src/main.rs
+++ topics/variables/src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
     // Types can be inferred
     let name = "Hai";
+
+    // This variable is immutable (act like a const in other languages)
+    // That means the following will fail:
+    //     name = "John";
+    
     let weight_pound = 150.0;
     let weight_kg = weight_pound / 2.2;
 
