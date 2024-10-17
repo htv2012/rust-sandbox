f163b4c90f1b30da39b76fc3f35fcb8b252efe1a
Hai Vu <haivu2004@gmail.com>
2024-10-17 15:56:12 -0700

    Added nested if conditional

--- topics/conditional/src/main.rs
+++ topics/conditional/src/main.rs
@@ -6,12 +6,24 @@ fn main() {
         println!("Not proceeding");
     }
 
-    let height = 161;
-    if height > 180 {
-        println!("{} is considered tall", height);
-    } else if height > 160 {
-        println!("{} is considered average", height);
-    } else {
-        println!("{} is considered short", height);
+    let height = 156;
+    let gender = "female";
+    if gender == "male" {
+
+        if height > 180 {
+            println!("{}cm is considered tall for a male", height);
+        } else if height > 160 {
+            println!("{}cm is considered average for a male", height);
+        } else {
+            println!("{}cm is considered short for a male", height);
+        }
+    } else if gender=="female" {
+        if height > 155 {
+            println!("{}cm is considered tall for a female", height);
+        } else if height > 135 {
+            println!("{}cm is considered average for a female", height);
+        } else {
+            println!("{}cm is considered short for a female", height);
+        }
     }
 }
