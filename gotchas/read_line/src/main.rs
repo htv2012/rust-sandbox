f05857f947a69831c94dcbd4fd6bd945478ddc07
Hai Vu <haivu2004@gmail.com>
2024-10-18 23:05:51 -0700

    Update doc for read_line

--- gotchas/read_line/src/main.rs
+++ gotchas/read_line/src/main.rs
@@ -1,3 +1,8 @@
+//! As a new Rustacean, I did not know that the `read_line()` function
+//! appends to the string buffer instead of clear-and-store. This crate
+//! demonstrate the wrong way and a couple of right ways to read string
+//! information from stdin.
+
 use std::io::Write;
 fn main() {
     wrong_way();
@@ -5,13 +10,10 @@ fn main() {
     right_way2();
 }
 
-
 /// This is the wrong way to do it `read_line()` keep appending to the string
 /// Secondly, the string will end with a newline character which we need to
 /// call `trim()` to remove it
-
 fn wrong_way() {
-
     println!("\n# Wrong Way to read_line()");
 
     let mut name = String::new();
@@ -25,7 +27,6 @@ fn wrong_way() {
 
 /// Right way #1: Use a fresh string every time we call `read_line()`
 fn right_way1() {
-
     println!("\n# Right Way #1: Use a fresh string every time we call read_line()");
     for _ in 0..3 {
         let mut name = String::new();
@@ -38,7 +39,6 @@ fn right_way1() {
 
 /// Right way #2: Clear the string before we call `read_line()`
 fn right_way2() {
-
     println!("\n# Right way #2: Clear the string before we call read_line()");
 
     let mut name = String::new();
@@ -49,5 +49,4 @@ fn right_way2() {
         std::io::stdin().read_line(&mut name).unwrap();
         println!("Hello, '{}'", name.trim())
     }
-
 }
