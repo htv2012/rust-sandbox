b23f05f25366531bf715eba9c624e0b759ba70b5
Hai Vu <haivu2004@gmail.com>
2024-10-23 11:06:16 -0700

    Rename function argument in ownership-function

--- ownership-function/src/main.rs
+++ ownership-function/src/main.rs
@@ -12,10 +12,10 @@ fn main() {
     // println!("Hello, {name}");
 }
 
-fn say_hello(name: String) {
-    // `say_hello()` takes ownership of `name`
-    println!("Hello, {name}");
+fn say_hello(the_name: String) {
+    // `say_hello()` takes ownership of `the_name`
+    println!("Hello, {the_name}");
 
-    // When `say_hello()` goes out of scope, so is `name`,
+    // When `say_hello()` goes out of scope, so is `the_name`,
     // and Rust will call a special function `drop()` to reclaim memory
 }
