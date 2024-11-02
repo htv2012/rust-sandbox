d6c3f90f02223e8234b64eadbf0e8e2c13c1230d
Hai Vu <haivu2004@gmail.com>
2024-11-01 19:41:03 -0700

    Add new

---
+++ concepts/struct/new/src/main.rs
@@ -0,0 +1,42 @@
+struct User {
+    username: String,
+    email: String,
+    uri: String,
+    active: bool,
+}
+
+impl User {
+    fn new(username: String, email: String, uri: String) -> Self {
+        Self {
+            username,
+            email,
+            uri,
+            active: true,
+        }
+    }
+
+    fn deactivate(&mut self) {
+        self.active = false;
+    }
+
+    fn print(&self) {
+        println!("User Name: {}", self.username);
+        println!("Email: {}", self.email);
+        println!("Uri: {}", self.uri);
+        println!("Active: {}", self.active);
+    }
+}
+
+fn main() {
+    let mut user = User::new(
+        String::from("anna"),
+        String::from("anna@company.com"),
+        String::from("https://about.me/anna"),
+    );
+    println!("\n# New User");
+    user.print();
+
+    println!("\n# Deactivate");
+    user.deactivate();
+    user.print();
+}
