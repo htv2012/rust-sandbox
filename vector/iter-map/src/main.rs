4938955f9e64bf369595be8c3fb413ae80865a0f
Hai Vu <haivu2004@gmail.com>
2024-11-08 11:09:07 -0800

    Add circumferences to iter-map

--- vector/iter-map/src/main.rs
+++ vector/iter-map/src/main.rs
@@ -1,11 +1,24 @@
+#[derive(Debug)]
 enum Shape {
     Circle(f64),
     Square(f64),
 }
 
+impl Shape {
+    fn circumference(&self) -> f64 {
+        match self {
+            Shape::Circle(radius) => radius * 2.0 * std::f64::consts::PI,
+            Shape::Square(side) => side * 4.0,
+        }
+    }
+}
+
 fn main() {
     let shapes = vec![Shape::Circle(5.0), Shape::Square(1.0)];
+    println!("\n# Vector");
+    println!("{:?}", shapes);
 
+    println!("\n# Total Areas");
     let total_area: f64 = shapes
         .iter()
         .map(|shape| match shape {
@@ -13,6 +26,9 @@ fn main() {
             Shape::Square(side) => side * side,
         })
         .sum();
+    println!("{:.2}", total_area);
 
-    println!("Total area: {}", total_area);
+    println!("\n# Total Circumferences");
+    let total_circumferences: f64 = shapes.iter().map(|shape| shape.circumference()).sum();
+    println!("{:.2}", total_circumferences);
 }
