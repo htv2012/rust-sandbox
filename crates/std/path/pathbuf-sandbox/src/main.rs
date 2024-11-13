7f83e846fad0490506a6a6bafa33870ddc9a79b4
Hai Vu <haivu2004@gmail.com>
2024-11-12 21:30:14 -0800

    Add more methods pathbuf-sandbox

--- crates/std/path/pathbuf-sandbox/src/main.rs
+++ crates/std/path/pathbuf-sandbox/src/main.rs
@@ -1,11 +1,20 @@
 use std::path::PathBuf;
 
 fn main() {
-    from_str();
+    let path = PathBuf::from("./src/main.rs");
+    show_attributes(&path);
+
+    let mut path = PathBuf::new();
+    path.push("/");
+    path.push("tmp");
+    show_attributes(&path);
+
+    path.push("file");
+    let path2 = path.with_extension("txt");
+    show_attributes(&path2);
 }
 
-fn from_str() {
-    let path = PathBuf::from("./src/main.rs");
+fn show_attributes(path: &PathBuf) {
     println!("\n# path={}", path.display());
 
     match path.canonicalize() {
@@ -13,7 +22,15 @@ fn from_str() {
         Err(error) => println!("canonicalize() -> error: {}", error),
     };
 
+    println!("ends_with(main.rs) -> {}", path.ends_with("main.rs"));
+    println!("ends_with(rs) -> {}", path.ends_with("rs"));
     println!("exists() -> {}", path.exists());
+    println!("extension() -> {:?}", path.extension());
+    println!("file_name() -> {:?}", path.file_name().unwrap());
+    println!("file_stem() -> {:?}", path.file_stem().unwrap());
+    println!("has_root() -> {}", path.has_root());
     println!("is_dir() -> {}", path.is_dir());
     println!("is_file() -> {}", path.is_file());
+    println!("is_relative() -> {}", path.is_relative());
+    println!("is_symlink() -> {}", path.is_symlink());
 }
