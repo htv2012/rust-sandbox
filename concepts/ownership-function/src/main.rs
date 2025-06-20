469c8b25028968c3cf5b55c336135acbd9d18606
Hai Vu <haivu2004@gmail.com>
2025-06-19 17:06:15 -0700

    update ownership-function

--- concepts/ownership-function/src/main.rs
+++ concepts/ownership-function/src/main.rs
@@ -2,20 +2,30 @@
 
 fn main() {
     // name is owned by function main
-    let name = String::from("world");
+    let name = String::from("Dolly");
 
-    // By passing `name` to `say_hello()`, `main()` no longer own `name`
-    // In other word, ownership has been moved from `main()` to `say_hello()`
-    say_hello(name);
+    // We tell say_hello that we will let it borrow name for a moment
+    say_hello(&name);
+    // After the function returned, we again own the variable name
+
+    // By passing `name` to `say_bye()`, `main()` no longer own `name`
+    // In other word, ownership has been moved from `main()` to `say_bye()`
+    say_bye(name);
 
     // That means this will fail:
     // println!("Hello, {name}");
 }
 
-fn say_hello(the_name: String) {
-    // `say_hello()` takes ownership of `the_name`
-    println!("Hello, {the_name}");
+fn say_hello(the_name: &String) {
+    // This function only 'borrow' the_name. It does not take ownership
+    // of the variable
+    println!("Hello {the_name}");
+}
 
-    // When `say_hello()` goes out of scope, so is `the_name`,
+fn say_bye(the_name: String) {
+    // `say_bye()` takes ownership of `the_name`
+    println!("Bye {the_name}");
+
+    // When `say_bye()` goes out of scope, so is `the_name`,
     // and Rust will call a special function `drop()` to reclaim memory
 }
